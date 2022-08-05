7f3437b86a30e9c9e429a5820accc2bae9da12ea
Mintae <alsxo99@snu.ac.kr>
2022-08-05 15:22:03 +0900

    #1 기본 Mapping

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private Orderstatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Orderstatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(Orderstatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    private int orderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
